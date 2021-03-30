564b6aaf9887f1ac0ab84e215dd25b710f2be051
Joe Panetta <jlapnetta1681@icloud.com>
2021-03-30 00:12:21 -0400

    trying to commit

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ WIDTH, HEIGHT = 800, 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Joe's Hangman Game!!")
 
-
 # Fonts
 LETTER_FONT = pygame.font.SysFont('comicsans', 40)
 
@@ -28,28 +27,27 @@ for i in range(26):
     y = starty + ((i // 13) * (GAP + RADIUS * 2))
     letters.append([x, y, chr(A + i), True])
 
-
-
 # Game variables
 hangman_status = 0
 
 # Colors
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 # Game Loop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
+
 def draw():
     win.fill(WHITE)
-# Render buttons
+    # Render buttons
     for letter in letters:
         x, y, ltr, visible = letter
         if visible:
             pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
-            text = LETTER_FONT.render(ltr, 1, BLACK)
-            win.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
+            text = LETTER_FONT.render(ltr, True, BLACK)
+            win.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
 
     win.blit(images[hangman_status], (150, 100))
     pygame.display.update()
@@ -57,20 +55,17 @@ def draw():
 
 while run:
     clock.tick(FPS)
-    
+
     draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-             m_x, m_y = pygame.mouse.get_pos()
-             for letter in letters:
-                 x, y, ltr, visible = letter
-                 if visible:
-                    dis = math.sqrt((x - m_x)**2 + (y - m_y)**2)
+            m_x, m_y = pygame.mouse.get_pos()
+            for letter in letters:
+                x, y, ltr, visible = letter
+                if visible:
+                    dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
                     if dis < RADIUS:
                         letter[3] = False
-
-
-             
